8095d2edc180b9daa04b75f16b9694514bc7a44e
Abel VARGA <avarga@whitepages.com>
2018-09-03 23:40:00 +0200

    Fetch all link hrefs and add mutation obsevrer

--- src/cljs/iswyd/core.cljs
+++ src/cljs/iswyd/core.cljs
@@ -4,33 +4,44 @@
 ;; -------------------------
 ;; Changelog
 ;; TODO: Throttle recording
-;; TODO: Use mutation observer instead to record attribute changes
 
 (def dmp (js/diff_match_patch.))
 
 (def prev-html (atom ""))
 (def changelog (atom []))
 
+=
+(def styles (atom []))
+
 (defn add-to-changelog [patch, timestamp]
   (swap! changelog (fn [] (conj @changelog {:patch patch
                                             :timestamp timestamp}))))
 
-(defn init-changelog []
-  (. js/document addEventListener
-     'DOMSubtreeModified
-     (fn []
-       (let [next-html (.-innerHTML (aget js/document.children 0))
-             diff (. dmp patch-make @prev-html next-html)
-             patch (. dmp patch-toText diff)]
-         (add-to-changelog patch (. js/Date now))
-         (js/console.log (clj->js @changelog))
-         (reset! prev-html next-html)))
-     false))
+(def obs-conf #js {:attributes true
+                   :childList true
+                   :subtree true})
 
-;; -------------------------
-;; Styles
+(def doc-root (aget js/document.children 0))
 
-(def styles (atom []))
+(defn child-list? [mut]
+  (= (.-type mut) "childList"))
+
+(defn has-link? [mut]
+  (let [nodes (array-seq (.-addedNodes mut))]
+    (reduce (fn [acc node]
+              (if acc
+                acc
+                (= (.-localName node) "link")))
+            false
+            nodes)))
+
+(defn link-added? [mut-list]
+  (reduce (fn [acc mut]
+            (if acc
+              acc
+              (and (child-list? mut) (has-link? mut))))
+          false
+          mut-list))
 
 (defn store-style [url text]
   (swap! styles (fn [] (conj @styles [url text])))
@@ -48,14 +59,44 @@
     (. req open "get" url)
     (. req send)))
 
-(defn res-text [ev]
-  ev.target.responseText)
+(defn res-text [ev] ev.target.responseText)
 
-(defn save-style [style])
+(defn stored? [url]
+  (reduce (fn [acc link]
+            (if acc
+              acc
+              (= (first link) url)))
+          false
+          @styles))
 
 (defn get-style [url]
-  (get-req url (fn [ev] (store-style url (res-text ev)))))
+  (if-not (stored? url)
+    (get-req url (fn [ev] (store-style url (res-text ev))))))
+
+(defn get-styles [links]
+  (js/console.log (count links))
+  (if-not (empty? links)
+    (reduce (fn [_, link]
+              (get-style (link-src link)))
+            nil
+            links)))
+
+(defn change-handlr [mut-list]
+  (let [next-html (.-innerHTML doc-root)
+        diff (. dmp patch-make @prev-html next-html)
+        patch (. dmp patch-toText diff)]
+    (if (> (count patch) 0)
+      (add-to-changelog patch (. js/Date now)))
+    (js/console.log (clj->js @changelog))
+    (if (link-added? mut-list)
+      (get-styles (links)))
+    (reset! prev-html next-html)))
+
+(def obs (js/MutationObserver. change-handlr))
+
+(defn init-changelog []
+  (. obs observe doc-root obs-conf))
 
 (defn main []
-  (get-style (link-src (first (links))))
+  (get-styles (links))
   (init-changelog))
