18350435ec5c9c8d05d23b9754182ef9987f96e6
Abel Varga <avarga@hajtospincer.hu>
2018-09-25 22:09:07 +0200

    Lift changes as separate service

--- src/clj/iswyd/handler.clj
+++ src/clj/iswyd/handler.clj
@@ -2,23 +2,13 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [hiccup.page :refer [include-js include-css html5]]
-            [iswyd.middleware :refer [wrap-middleware]]
             [config.core :refer [env]]
-            [cheshire.core :as json]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
             [ring.middleware.cookies :refer [wrap-cookies]]
             [config.core :refer [env]]
-            [monger.core :as mg]
-            [monger.collection :as mc])
-  (:import (com.mongodb MongoOptions ServerAddress)
-           org.bson.types.ObjectId))
-
-(defonce conn (mg/connect {:host (env :mongo-host)}))
-(defonce db (mg/get-db conn "iswyd"))
-
-(defn uuid [] (str (java.util.UUID/randomUUID)))
+            [iswyd.utils :refer [uuid]]))
 
 (def mount-target
   [:div#app
@@ -40,23 +30,11 @@
     (include-js "/js/app.js")
     (include-js "/js/iswyd-lib.js")]))
 
-(defn api-raw-changes [handler]
-  (fn [req]
-    ;; (mc/insert db "changes" {:_id (ObjectId. (uuid))
-    ;;                            :session (:req (:cookies req))
-    ;;                            :data (slurp body) })
-    {:status 200
-     :body (json/generate-string
-            {:errors []
-             :success true})}))
-
 (defroutes app-routes
   (GET "/" [] (loading-page))
   (GET "/about" [] (loading-page))
 
-  (POST "/api/changes" req (api-raw-changes req))
-
-  ;;(resources "/")
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (defn wrap-session-id-cookie [handler]
