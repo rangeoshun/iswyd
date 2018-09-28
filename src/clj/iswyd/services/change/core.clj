a4a22136d8275787c4799e8645e66a41d5c74f68
Abel VARGA <avarga@whitepages.com>
2018-09-28 11:58:30 +0200

    Decode change before kafka

--- src/clj/iswyd/services/change/core.clj
+++ src/clj/iswyd/services/change/core.clj
@@ -1,36 +1,41 @@
 (ns iswyd.services.change.core
-  (:require [cheshire.core :as json]
+  (:require [clojure.data.json :as json]
             [clojure.core.async :as a :refer [go <! >!]]
             [clojure.tools.logging :as log]
             [config.core :refer [env]]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [kinsky.client :as client]
-            [kinsky.async       :as async]
-            [ring.middleware.cookies :as rm])
+            [kinsky.async :as async]
+            [ring.middleware.cookies :as rm]
+            [ring.util.codec :as ruc])
   (:import rufus.lzstring4java.LZString))
 
 ;; TODO: Find out what is the idiomatic way to handle errors
 
-(defonce p (client/producer {:bootstrap.servers (:kafka-host env)}
-                            (client/keyword-serializer)
-                            (client/edn-serializer)))
+(defonce ch (async/producer {:bootstrap.servers (:kafka-host env)} :keyword :edn))
 
-(defn decomp [data] (. LZString decompressFromBase64 data))
+(defn decode [data]
+  (json/read-str (. LZString decompressFromBase64 data)
+                 :value-fn (fn [key val] (if (string? val)
+                                          (ruc/percent-decode val)
+                                          val))
+                 :key-fn keyword))
 
 (defn pub-change [session-id data]
-  (client/send! p (:raw-topic env) :c {:session-id session-id
-                                       :data       data})
+  (go
+    (>! ch {:topic (:change-topic env) :key :change :value {:session-id session-id
+                                                            :data       data}}))
   {:status 200
-   :body   (json/generate-string {:success true})})
+   :body   (json/write-str {:success true})})
 
 (defn handle-srv-fail []
   {:status 500
-   :body   (json/generate-string {:success false})})
+   :body   (json/write-str {:success false})})
 
 (defn handle-ok [session-id data]
   (try
-    (let [data (decomp data)]
+    (let [data (decode data)]
       (if-not (nil? data)
         (pub-change session-id data)
         (handle-srv-fail)))
@@ -45,12 +50,12 @@
     (if (and session-id data)
       (handle-ok session-id data)
       {:status 400
-       :body   (json/generate-string {:success false})})))
+       :body   (json/write-str {:success false})})))
 
 (defroutes srv-routes
   (POST "/" request (change-handler request))
   (route/resources "/")
   (route/not-found {:status 405
-                    :body   (json/generate-string {:success false})}))
+                    :body   (json/write-str {:success false})}))
 
 (def main (rm/wrap-cookies srv-routes))
